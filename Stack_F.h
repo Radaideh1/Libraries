414c599c42f036d03cd4bd3d8e28949faf26f318
A_Radaideh <radaideh7484@outlook.com>
2026-04-18 16:11:19 +0300

    feat: enhance stack with string-specific template

--- Stack_F.h
+++ Stack_F.h
@@ -6,45 +6,25 @@ using namespace std;
 
 template <class T>
 class Stack {
-private:
-
-    struct Node {
-        T element;
-        Node* next;
-
-        Node(T ele) :element(ele), next(nullptr) {}
-    };
-
-    Node* top;
-    int length;
-
-    bool _isEmpty() const {
-        return top == nullptr;
-    }
-
-
-
 public:
 
-    // Constructor
+    // Constructor && Destructor
     Stack() :top(nullptr), length(0) {}
-
-    // Destructor
+ 
     ~Stack() {
         while (!_isEmpty()) {
             pop();
         }
     }
 
-    // Push
+
+    
     void push(T ele) {
         Node* newNode = new Node(ele);
         newNode->next = top;
         top = newNode;
         length++;
     }
-
-    // Pop
     T pop() {
         if (_isEmpty()) {
             throw  runtime_error("Stack is empty");
@@ -59,24 +39,21 @@ public:
 
         return value;
     }
-
-    // Top
     T getTop() const {
         if (_isEmpty()) throw runtime_error("Stack is empty");
         return top->element;
     }
 
-    // isEmpty (public إذا احتجته)
+   
+    
     bool isEmpty() const {
         return top == nullptr;
     }
-
-    // Size
+    
     int size() const {
         return length;
     }
 
-    
     void display() const {
         Node* temp = top;
         cout << "[ ";
@@ -87,4 +64,116 @@ public:
         }
         cout <<" ]" << endl;
     }
+
+
+private:
+
+    struct Node {
+        T element;
+        Node* next;
+
+        Node(T ele) :element(ele), next(nullptr) {}
+    };
+
+    Node* top;
+    int length;
+    bool _isEmpty() const {
+        return top == nullptr;
+    }
+
+};
+
+
+
+template<>
+class Stack<string> {
+public:
+
+    // Constructor & Destructor
+    Stack() : top(nullptr), length(0) {}
+
+    ~Stack() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+
+    void push(const string& ele) {
+        Node* newNode = new Node(ele);
+        newNode->next = top;
+        top = newNode;
+        length++;
+    }
+
+    string pop() {
+        if (isEmpty()) {
+            throw runtime_error("Stack is empty");
+        }
+
+        Node* temp = top;
+        string value = temp->element;
+
+        top = top->next;
+        delete temp;
+        length--;
+
+        return value;
+    }
+
+    string getTop() const {
+        if (isEmpty())
+            throw runtime_error("Stack is empty");
+
+        return top->element;
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+
+    int size() const {
+        return length;
+    }
+
+    void display() const {
+        Node* temp = top;
+
+        cout << "[ ";
+        while (temp != nullptr) {
+            cout << "\"" << temp->element << "\"";  // مهم للتمييز
+            if (temp->next) cout << " , ";
+            temp = temp->next;
+        }
+        cout << " ]" << endl;
+    }
+
+    bool isOperator(const string& op) const {
+        return op == "+" || op == "-" || op == "*" || op == "/";
+    }
+
+    string clearOperator() {
+        string temp = "";
+        while (getTop() != "(") {
+
+            temp += pop();
+            temp += " ";
+
+        }
+        pop();
+        return temp;
+
+    }
+
+
+private:
+
+    struct Node {
+        string element;
+        Node* next;
+
+        Node(const string& ele) : element(ele), next(nullptr) {}
+    };
+
+    Node* top;
+    int length;
 };
